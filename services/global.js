f1f7d610ff724ba29d4ad230c25a26b7ef472cda
May Haham <mayhaham@gmail.com>
2025-05-09 21:29:23 +0300

    תפריט

--- services/global.js
+++ services/global.js
@@ -1,23 +1,20 @@
 var hamburger = document.getElementById("hamburger");
 var navLinks = document.getElementById("nav-links");
-
-hamburger.onclick = function () {
-  if (navLinks.className === "nav-links") {
-    navLinks.className = "nav-links nav-open";
-  } else {
-    navLinks.className = "nav-links";
-  }
-};
-
 var darkToggle = document.getElementById("dark-mode-toggle");
 var body = document.body;
+
+hamburger.onclick = function () {
+  if (navLinks.style.display === "flex") {
+    navLinks.style.display = "none";
+  } else {
+    navLinks.style.display = "flex";
+  }
+};
 
 darkToggle.onclick = function () {
   if (body.className === "dark-mode") {
     body.className = "";
-    darkToggle.innerHTML = '<i class="fas fa-moon moon-icon"></i>';
   } else {
     body.className = "dark-mode";
-    darkToggle.innerHTML = '<i class="fas fa-sun sun-icon"></i>';
   }
 };
