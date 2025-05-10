85029ff0192f5a6979fde842007d33bf464d305c
May Haham <mayhaham@gmail.com>
2025-05-10 14:50:12 +0300

    תיקון האייקון בין לילה ליום

--- services/global.js
+++ services/global.js
@@ -2,6 +2,7 @@ var hamburger = document.getElementById("hamburger");
 var navLinks = document.getElementById("nav-links");
 var darkToggle = document.getElementById("dark-mode-toggle");
 var body = document.body;
+var icon = darkToggle.querySelector("i");
 
 hamburger.onclick = function () {
   if (navLinks.style.display === "flex") {
@@ -14,7 +15,9 @@ hamburger.onclick = function () {
 darkToggle.onclick = function () {
   if (body.className === "dark-mode") {
     body.className = "";
+    icon.className = "fas fa-moon moon-icon";
   } else {
     body.className = "dark-mode";
+    icon.className = "fas fa-sun sun-icon";
   }
 };
